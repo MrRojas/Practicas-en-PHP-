8b19d3e27034371ee9033a6640ca265d93a367dc
= <=>
2018-07-08 21:20:08 -0400

    update

---
+++ funcion_date/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+
+/** Error reporting */
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+
+define('EOL',(PHP_SAPI == 'cli') ? PHP_EOL : '<br />');
+
+date_default_timezone_set('Europe/London');
+echo date('H:i:s') , " Load from Excel5 template" , EOL;
+
+ ?>
